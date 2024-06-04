1599b6a613c001b116292207907dbc5b4ab6487f
Kishor <124120029+moltresIn@users.noreply.github.com>
2024-06-04 10:15:22 +0000

    installing react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './header/navbar'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline ">
+        Hello world!
+      </h1>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/header/navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div>
+            <h1 className="" >Hello There</h1>
+        </div>
+    )
+}
